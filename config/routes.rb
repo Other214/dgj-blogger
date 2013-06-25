9bdf32a7ee9f730b6e6ab3efe942a0d1331ba758
Donald Jones <other214@gmail.com>
2013-06-25 13:20:11 -0400

    completed authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Blogger::Application.routes.draw do
   end
   resources :tags
   resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  match 'login'  => 'author_sessions#new'
+  match 'logout' => 'author_sessions#destroy'
+  
 end
